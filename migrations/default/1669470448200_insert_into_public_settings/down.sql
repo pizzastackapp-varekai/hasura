ed09289e8b1333e0962367c927cacbf23f8fc9e4
Serhii <s.saviuk@apiko.com>
2022-12-05 18:30:10 +0200

    get categories

---
+++ migrations/default/1669470448200_insert_into_public_settings/down.sql
@@ -0,0 +1,1 @@
+DELETE FROM "public"."settings" WHERE "id" = '9729c237-a943-4d17-8979-aa9b6475b2af';
---
+++ migrations/default/1669470448200_insert_into_public_settings/up.sql
@@ -0,0 +1,1 @@
+INSERT INTO "public"."settings"("id", "drinks_category") VALUES (E'9729c237-a943-4d17-8979-aa9b6475b2af', null);
